4bb40cc3732b39d2bd86fc24f1966ba2ead0bef6
Alexroot <alexroot@list.ru>
2025-02-10 15:50:23 +0300

    app init

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+	// Императивный стиль: использование хука useState для управления состоянием
+	const [count, setCount] = useState(0)
+
+	// Декларативный стиль: описание интерфейса
+	return (
+		<>
+			<div>
+				<a href="https://vite.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				{/* Императивный стиль: обработчик события onClick */}
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+			{/* Императивный стиль: определение текущего года */}
+			<p className="year">{new Date().getFullYear()}</p>
+		</>
+	)
+}
